2a518d192e284d448b96a8f8527dd79a56fcb4c3
bourahma <az.az2012221@gmail.com>
2023-10-08 20:11:04 +0200

    ShopLoc : Fixing ci issue

--- shoploc-be/src/test/java/com/mimka/shoplocbe/ShoplocBeApplicationTests.java
+++ shoploc-be/src/test/java/com/mimka/shoplocbe/ShoplocBeApplicationTests.java
@@ -2,12 +2,13 @@ package com.mimka.shoplocbe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class ShoplocBeApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
