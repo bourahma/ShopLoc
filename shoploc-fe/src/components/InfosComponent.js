cac29ada27bacb6c8b2f08129cd857265a6eaa7b
bourahma <az.az2012221@gmail.com>
2023-10-23 09:35:24 +0200

    fe - add welcome page

---
+++ shoploc-fe/src/components/InfosComponent.js
@@ -0,0 +1,13 @@
+import './InfosComponent.css';
+
+const InfosComponent = () => {
+
+  return (
+    <div id='legacy-info'>
+        <div id='shfe-logo'> <img src='./shoploc-logo.png' alt="Gain de points"/></div>
+        <div> <img id='shfe-spg' src='./shoploc-points-gain.png' alt="Gain de points"/></div>
+    </div>
+  );
+};
+
+export default InfosComponent;
