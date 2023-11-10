cc49e76669f3c34ecfd06597904f3a04a1416d9a
arnaud kaderi <arnay07@gmail.com>
2023-11-10 14:51:44 +0100

    formulaire de connexion en cours

--- shoploc-fe/src/App.js
+++ shoploc-fe/src/App.js
@@ -1,14 +1,28 @@
-
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomeComponent from './components/home/HomeComponent';
+import React from "react";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import HomeComponent from "./components/HomeComponent";
+import AuthComponent from "./components/AuthComponent";
+import Header from "./components/Header";
+import LegacyInfosComponent from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomeComponent />}>
+        <Route
+          path="/"
+          element={
+            <div className="flex flex-col min-h-screen">
+              <div className="flex-1">
+                <Header />
+                <Outlet />
+              </div>
+              <LegacyInfosComponent className="bg-gray-300 py-4 text-center" />
+            </div>
+          }
+        >
           <Route index element={<HomeComponent />} />
+          <Route path="login" element={<AuthComponent />} />
         </Route>
       </Routes>
     </BrowserRouter>
