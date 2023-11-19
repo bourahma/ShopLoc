83964ac6e52ecbdc2957db433a95d9c1d50b7716
islam bouterbiat <islambouter2000@gmail.com>
2023-11-19 23:53:38 +0100

    edited products and merchant  page + added some products data

--- shoploc-fe/src/App.js
+++ shoploc-fe/src/App.js
@@ -7,28 +7,34 @@ import Products from "./components/Products";
 import LegacyInfosComponent from "./components/Footer";
 
 function App() {
-  return (
-      <BrowserRouter>
-          <Routes>
-              <Route
-                  path="/"
-                  element={
-                      <div className="flex flex-col min-h-screen">
-                          <div className="flex-1">
-                              <Header />
-                              <Outlet />
-                          </div>
-                          <LegacyInfosComponent className="bg-gray-300 py-4 text-center" />
-                      </div>
-                  }
-              >
-                  <Route index element={<HomeComponent />} />
-                  <Route path="login" element={<AuthComponent />} />
-                  <Route path="/commercant/:id" element={<Products />} />
-              </Route>
-          </Routes>
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <div className="flex flex-col min-h-screen">
+                            <div className="flex-1">
+                                <Header />
+                                <div className="px-20 py-8">
+
+                                <Outlet />
+                                </div>
+                            </div>
+                            <LegacyInfosComponent className="bg-gray-300 py-4 text-center" />
+                        </div>
+                    }
+                >
+                    <Route index element={<HomeComponent />} />
+                    <Route path="login" element={<AuthComponent />} />
+                    <Route
+                        path="/commercant/:commercantId"
+                        element={<Products />}
+                    />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- shoploc-fe/src/components/Products.jsx
+++ shoploc-fe/src/components/Products.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
-// Importez vos styles Tailwind et les autres composants nécessaires
-
 const Product = () => {
     const { commercantId } = useParams();
     console.log(commercantId);
-    const dummyData = require("../utils/commerçants.json"); // Remplacez le chemin par le bon chemin
+    const dummyData = require("../utils/commerçants.json");
     const commercant = dummyData.commercants.find(
         (f) => f.id === parseInt(commercantId)
     );
@@ -19,41 +17,41 @@ const Product = () => {
         <div className="container mx-auto mt-8">
             <div className="flex items-center mb-4">
                 <img
-                    src={commercant.image}
+                    src={require(`../images/${commercant.image}`)}
                     alt={commercant.nom}
                     className="w-16 h-16 object-cover rounded-full mr-4"
                 />
                 <div>
-                    <h1 className="text-2xl font-semibold">
-                        {commercant.nom}
-                    </h1>
+                    <h1 className="text-2xl font-semibold">{commercant.nom}</h1>
                     <p className="text-gray-600">{commercant.description}</p>
                 </div>
             </div>
             <h2 className="text-xl font-semibold mb-4">Produits</h2>
-            <div className="grid grid-cols-2 gap-4">
+            <div className="grid grid-cols-4 gap-4">
                 {commercant.produits.map((produit) => (
                     <div
                         key={produit.id}
-                        className="p-4 border border-gray-300 rounded"
+                        className="border border-gray-300 rounded"
                     >
                         <img
-                            src={produit.image}
+                            src={require(`../images/${produit.image}`)}
                             alt={produit.nom}
-                            className="w-full h-32 object-cover mb-2"
+                            className="w-full h-52 object-contain mb-2"
                         />
-                        <h3 className="text-lg font-semibold mb-1">
-                            {produit.nom}
-                        </h3>
-                        <p className="text-gray-600 mb-2">
-                            {produit.description}
-                        </p>
-                        <p className="text-gray-700">
-                            Prix: ${produit.prix.toFixed(2)}
-                        </p>
-                        <p className="text-gray-700">
-                            En stock: {produit.stock}
-                        </p>
+                        <div className="p-4">
+                            <h3 className="text-lg font-semibold mb-1">
+                                {produit.nom}
+                            </h3>
+                            <p className="text-gray-600 mb-2">
+                                {produit.description}
+                            </p>
+                            <p className="text-gray-700">
+                                Prix: ${produit.prix.toFixed(2)}
+                            </p>
+                            <p className="text-gray-700">
+                                En stock: {produit.stock}
+                            </p>
+                        </div>
                     </div>
                 ))}
             </div>
