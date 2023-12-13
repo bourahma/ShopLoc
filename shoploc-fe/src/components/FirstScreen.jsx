81787403284faff67107eee1077b68035fb9022a
islam bouterbiat <islambouter2000@gmail.com>
2023-12-13 15:08:11 +0100

    small modif on design

--- shoploc-fe/src/components/FirstScreen.jsx
+++ shoploc-fe/src/components/FirstScreen.jsx
@@ -7,11 +7,11 @@ import offre_img from "../images/offre_1.png";
 function FirstScreen() {
     
     return (
-        <div className="grid md:grid-cols-12 grid-cols-3 md:gap-20 gap-4 my-12">
-            <div className="md:col-span-3 col-span-3 bg-shopgray order-4 md:order-1 relative flex flex-col">
-                <NewsSlider/>
+        <div className="grid md:grid-cols-12 grid-cols-3 md:gap-12 gap-4 my-12">
+            <div className="md:col-span-3 col-span-3 bg-shopgray order-4 md:order-1 relative flex flex-col rounded">
+                <NewsSlider />
             </div>
-            <div className="md:col-span-5 col-span-3 order-3 md:order-2">
+            <div className="md:col-span-5 col-span-3 order-3 md:order-2 rounded">
                 <Map />
             </div>
             <div className="md:col-span-2 col-span-1 bg-shopgray flex justify-center items-center rounded order-1 md:order-3">
