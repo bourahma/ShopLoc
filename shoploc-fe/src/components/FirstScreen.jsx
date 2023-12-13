1c2e29f74ecf51de3369881a6ccbdc37272fda7a
islam bouterbiat <islambouter2000@gmail.com>
2023-12-13 15:01:35 +0100

    added news slider for france component with real api call

--- shoploc-fe/src/components/FirstScreen.jsx
+++ shoploc-fe/src/components/FirstScreen.jsx
@@ -1,17 +1,17 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import { Map } from "./Map";
-import offre_img from "../images/offre_1.png";
+import NewsSlider from './NewsSlider';
 
+import offre_img from "../images/offre_1.png";
 
 function FirstScreen() {
     
     return (
         <div className="grid md:grid-cols-12 grid-cols-3 md:gap-20 gap-4 my-12">
-            <div className="md:col-span-2 bg-shopgray justify-center items-center hidden md:flex md:order-1">
-                Pub Area
+            <div className="md:col-span-3 col-span-3 bg-shopgray order-4 md:order-1 relative flex flex-col">
+                <NewsSlider/>
             </div>
-            <div className="md:col-span-5 col-span-3 order-last md:order-2">
+            <div className="md:col-span-5 col-span-3 order-3 md:order-2">
                 <Map />
             </div>
             <div className="md:col-span-2 col-span-1 bg-shopgray flex justify-center items-center rounded order-1 md:order-3">
@@ -19,7 +19,7 @@ function FirstScreen() {
                     <img src={offre_img} alt="" />
                 </div>
             </div>
-            <div className="md:col-span-3 col-span-2 flex flex-col gap-10 items-center justify-center md:justify-start order-2 md:order-4">
+            <div className="md:col-span-2 col-span-2 flex flex-col gap-10 items-center justify-center md:justify-start order-2 md:order-4">
                 <Link to="/login">
                     <button className="bg-shopred text-white font-bold px-6 py-2 rounded text-center w-48 ">
                         Se connecter
